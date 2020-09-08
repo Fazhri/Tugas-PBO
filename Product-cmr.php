5a97d5e37cd6aa7200da53fc87c0f7dc79572caa
Fazhriii <69241770+Fazhri@users.noreply.github.com>
2020-09-08 16:13:35 +0700

    Add files via upload

---
+++ Product-cmr.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+@return \Illuminate\Http\Response
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product-cmr extends Model
+public function index ()
+{
+    //ini tampilan index
+    return view('product.index');
+}
